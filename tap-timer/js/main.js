c7e4a32f132589b517240a8a842ffea26e15d5de
gen <genice6776@hotmail.com>
2024-08-25 01:10:59 -0400

    styling of grid and circular

--- tap-timer/js/main.js
+++ tap-timer/js/main.js
@@ -1,15 +1,19 @@
+
+// VARIABLES
 const classicLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','V','W'];
 // const classicLetters = ['A','B','C','D','E'];
 let leftoverLetters;
 
+const game = document.getElementById("game");
 const gameEnding = document.getElementById("lose-screen");
 const gameSettings = document.getElementById("settings-wrapper");
 
 const letterList = document.getElementById("letters-list");
+const layoutSetting = document.getElementById("layout-setting");
 
 const selectedCategory = document.getElementById("category");
 const categoriesSetting = document.getElementById("category-setting");
-var showCategories = false;
+var showCategories = true;
 
 const timerSetting = document.getElementById("timer-setting");
 const timer = document.getElementById("timer");
@@ -25,6 +29,7 @@ const playerList = [{
 }];
 
 
+// DOCUMENT SETUP
 $(document).ready(function(){
   
   timerSetting.defaultValue = 10;
@@ -32,20 +37,39 @@ $(document).ready(function(){
   generateCategory();
   generateGame();
   
-  console.log("on page load", maxTime);
+  // console.log("on page load", maxTime);
   
 });
 
 // Game functions
 function generateGame() {
   let n = 0;
-  console.log("generate time", maxTime);
+  // console.log("generate time", maxTime);
+
+  layout = layoutSetting.value;
+  
+  console.log("layout is ", layout);
+
+  if (layout == 'circle') {
+    game.classList = "circular";
+  } else {
+    game.classList = "grid";
+  }
+
+
   while (n < classicLetters.length) {
+    const letterWrapper = document.createElement("div");
     const letter = document.createElement("button");
+
+    letterWrapper.classList.add('letter-wrapper');
+    letterWrapper.setAttribute('style', rearrangeLetters(layout, n));
+
     letter.setAttribute('onclick','selectLetter(this, "' + classicLetters[n] + '")');
     letter.classList.add('letter');
     letter.innerText = classicLetters[n];
-    letterList.appendChild(letter);
+
+    letterWrapper.appendChild(letter);
+    letterList.appendChild(letterWrapper);
     n++;
   }
   
@@ -60,7 +84,7 @@ function resetGame() {
   letterList.innerText = "";
   gameEnding.classList.add('hide');
   
-  console.log("Reset time", maxTime);
+  // console.log("Reset time", maxTime);
   
   generateGame();
 }
@@ -80,6 +104,7 @@ function generateCategory() {
   selectedCategory.innerHTML = categories[n];
 }
 
+// Letters
 function selectLetter(elem, letter) {
   elem.classList.add('selected');
   elem.setAttribute('disabled', 'true');
@@ -95,6 +120,25 @@ function selectLetter(elem, letter) {
   
   clearInterval(timerRef);
   startTimer();
+}
+
+function rearrangeLetters(layout, index) {
+  switch (layout) {
+    case 'circle':
+      return calculateAngle(index);
+      break;
+    case 'grid':
+      return "transform: rotate(0deg)";
+    default:
+      return "transform: rotate(0deg)";
+      break;
+  }
+}
+
+function calculateAngle(index) {
+  var angle = 360 / classicLetters.length * index;
+  console.log("calculate", angle);
+  return "transform: rotate(" + angle + "deg)";
 }
 
 // Timer
@@ -120,8 +164,6 @@ function openGameSettings() {
   gameSettings.classList.toggle('hide');
 }
 
-
-
 function closeSettings() {
   gameSettings.classList.toggle('hide');
   var form = document.getElementById("settings");
@@ -136,11 +178,11 @@ function saveSettings() {
   return closeSettings();
 }
 
-
 function updateGameSettings() {
   maxTime = timerSetting.value;
-  showCategories = categoriesSetting.value;
-  showPlayer = playerSetting.value;
+  showCategories = categoriesSetting.checked;
+  showPlayer = playerSetting.checked;
+  letterArrangement = layoutSetting.value;
 
   if (maxTime) {
     console.log("Time", maxTime);
