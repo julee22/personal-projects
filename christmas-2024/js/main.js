7478dae2176a592ab56fe453313f1673757a548b
gen <genice6776@hotmail.com>
2024-11-21 00:10:47 -0500

    fixed buttons

--- christmas-2024/js/main.js
+++ christmas-2024/js/main.js
@@ -5,38 +5,82 @@ var allCopy = [{
   copy: 'Mission Details'
 }, {
   id: 'mission-brief',
-  copy: 'Your mission should you choose to accept it, is to attend a gathering of secret agents on Christmas Eve and discover the true meaning of Christmas.'
+  copy: 'Your mission, should you choose to accept it, is to attend a gathering of secret agents on Christmas Eve and discover the true meaning of Christmas.'
 }, {
-  id: 'mission-details-location',
-  copy: 'Location: 32 St James Ave'
+  id: 'mission-loc',
+  copy: 'Location:'
 }, {
-  id: 'mission-details-date',
-  copy: 'Date: 24-12-2024'
+  id: 'mission-loc-details',
+  copy: '32 St James Ave'
+},{
+  id: 'mission-date',
+  copy: 'Date:'
 }, {
-  id: 'mission-details-time',
-  copy: 'Time: 19H00'
+  id: 'mission-date-details',
+  copy: '24-12-2024'
+}, {
+  id: 'mission-time',
+  copy: 'Time:'
+}, {
+  id: 'mission-time-details',
+  copy: '19H00'
+}, {
+  id: 'mission-accept',
+  copy: 'Do you accept?'
 }
 ]
 
 var copyIndex = 0;
 var i = 0;
-var speed = 100;
+var speed = 5;
 
+var showMission = false;
+var showForm = false;
 
+const agentName = document.getElementById('name');
+const landing = document.getElementById("landing");
+const mission = document.getElementById("mission");
+const question = document.getElementById("question");
+const successMessage = document.getElementById("success-msg");
+
+const timer = document.getElementById("timer");
+var timerRef;
+var currTime = 10;
+var maxTime = 10;
 
 // DOCUMENT SETUP
 $(document).ready(function() {
-  typeWriter(allCopy[0].id, allCopy[0].copy);
   
+  agentName.addEventListener("input", function(e) {
+    if (agentName.value != '') {
+      document.getElementById('goBtn').classList.add("show");
+    }
+  });
+  
+  const form = document.getElementById('my-form');
+  const success_msg = document.getElementById('success-msg');
+
+  form.reset();
+
+  // form submission to Google Sheets
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      // what to show after submission
+      success_msg.classList.toggle('hide');
+      form.style.display = "none";
+      startTimer();
+    })
+  });
 });
 
-function nextCopy() {
-  if (copyIndex < allCopy.length) {
-    typeWriter(allCopy[copyIndex].id, allCopy[copyIndex].copy);
-  }
-}
-
-
+// typewriter effect
 function typeWriter(missionId, missionCopy) {
   var currCopy = document.getElementById(missionId).innerHTML;
 
@@ -52,4 +96,41 @@ function typeWriter(missionId, missionCopy) {
     copyIndex++;
     nextCopy();
   }
+}
+
+// used to move on to the next paragraph
+function nextCopy() {
+  if (copyIndex < allCopy.length) {
+    typeWriter(allCopy[copyIndex].id, allCopy[copyIndex].copy);
+  } else {
+    showForm = true;
+    question.classList.toggle("hide");
+  }
+}
+
+function nameEntered() {
+  if (agentName.value != '') {
+    typeWriter(allCopy[0].id, allCopy[0].copy);
+    mission.classList.toggle("hide");
+    landing.classList.toggle("hide");
+  }
+}
+
+
+function startTimer() {
+  timer.innerHTML = maxTime;
+  index = 1;
+
+
+  timerRef = setInterval(function() {
+
+    if (index == maxTime) {
+      timer.innerHTML = "0";
+      clearInterval(timerRef);
+      window.location.replace("https://google.com");
+    } else {
+      timer.innerHTML = maxTime - index;
+      index++;
+    }
+  }, 1000);
 }
