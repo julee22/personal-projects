ab2076468ceefa0c67785155afd8f2798e23c68a
gen <genice6776@hotmail.com>
2026-08-17 01:02:22 -0400

    set up seating chart for reception

---
+++ michael-jessica/js/reception.js
@@ -0,0 +1,46 @@
+import { guestList } from "./seating-chart.js";
+
+let guestSearch;
+const sortedList = guestList.sort((a, b) => a.text.localeCompare(b.text));
+const tableNum = document.getElementById('tableNum');
+
+// Selected Guest
+let selectedGuest;
+let guestName;
+let guestTable;
+
+function populateGuestSearch() {
+  for (let index = 0; index < sortedList.length; index++) {
+    const newOption = document.createElement('option');
+
+    newOption.value = sortedList[index].id;
+    newOption.innerHTML = sortedList[index].text;
+
+    guestSearch.appendChild(newOption);
+  }
+}
+
+$(document).ready(function () {
+  guestSearch = document.getElementById('guestlist');
+  console.log(sortedList);
+  populateGuestSearch();
+  $('#guestlist').select2();
+
+  // register change listener after the select has been populated/initialized
+  $('#guestlist').on('change', function (event) {
+    const id = this.value;
+    selectedGuest = sortedList.find((element) => String(element.id) === String(id));
+
+    // Update table number
+    tableNum.innerHTML = selectedGuest.table;
+
+    // Update meal selection
+    // const mealSelection = document.getElementById('mealSelection');
+
+    const mainDiv = document.getElementById('main');
+    mainDiv.style.display = 'block';
+    
+    const welcomeDiv = document.getElementById('welcome');
+    welcomeDiv.classList.add('collapse');
+  });
+});
---
+++ michael-jessica/js/seating-chart.js
@@ -0,0 +1,440 @@
+export const guestList = [
+ {
+   id: 0,
+   table: 3,
+   text: "Stefany Jaya ",
+   meal: "Short-rib"
+ },
+ {
+   id: 1,
+   table: 3,
+   text: "Geoffrey Rota ",
+   meal: "Short-rib"
+ },
+ {
+   id: 2,
+   table: 3,
+   text: "Genice Chandra",
+   meal: "Short-rib"
+ },
+ {
+   id: 3,
+   table: 2,
+   text: "Lidya Susanto",
+   meal: "Short-rib"
+ },
+ {
+   id: 4,
+   table: 1,
+   text: "Eddy Ekasalim",
+   meal: "Short-rib"
+ },
+ {
+   id: 5,
+   table: 1,
+   text: "Lily Suryanto",
+   meal: "Salmon"
+ },
+ {
+   id: 6,
+   table: 1,
+   text: "Yanti Ekasalim",
+   meal: "Salmon"
+ },
+ {
+   id: 7,
+   table: 1,
+   text: "Dewi Ekasalim",
+   meal: "Short-rib"
+ },
+ {
+   id: 8,
+   table: 1,
+   text: "Henny Ekasalim",
+   meal: "Salmon"
+ },
+ {
+   id: 9,
+   table: 1,
+   text: "Yenny Ekasalim",
+   meal: "Salmon"
+ },
+ {
+   id: 10,
+   table: 4,
+   text: "Elizabeth S Melinda",
+   meal: "Salmon"
+ },
+ {
+   id: 11,
+   table: 8,
+   text: "Gavin Sayuto",
+   meal: "Short-rib"
+ },
+ {
+   id: 12,
+   table: 3,
+   text: "Adelia Yolanda",
+   meal: "Short-rib"
+ },
+ {
+   id: 13,
+   table: 5,
+   text: "Angela Susilo",
+   meal: "Short-rib"
+ },
+ {
+   id: 14,
+   table: 5,
+   text: "Adrian Hartanto",
+   meal: "Short-rib"
+ },
+ {
+   id: 15,
+   table: 7,
+   text: "Benvenuto Sean Akira",
+   meal: "Chicken"
+ },
+ {
+   id: 16,
+   table: 7,
+   text: "Jiahui Kang",
+   meal: "Salmon"
+ },
+ {
+   id: 17,
+   table: 7,
+   text: "Rui Kang",
+   meal: "Kid's meal"
+ },
+ {
+   id: 18,
+   table: 1,
+   text: "Indri Simanhadi ",
+   meal: "Short-rib"
+ },
+ {
+   id: 19,
+   table: 1,
+   text: "Dewi Amelia",
+   meal: "Salmon"
+ },
+ {
+   id: 20,
+   table: 6,
+   text: "Shierly xu",
+   meal: "Salmon"
+ },
+ {
+   id: 21,
+   table: 6,
+   text: "Lisandra legawa",
+   meal: "Salmon"
+ },
+ {
+   id: 22,
+   table: 6,
+   text: "Clyve Widjaya",
+   meal: "Short-rib"
+ },
+ {
+   id: 23,
+   table: 6,
+   text: "Jackson Michaels Widjaya",
+   meal: "None"
+ },
+ {
+   id: 24,
+   table: 6,
+   text: "Jessica Samiri",
+   meal: "Short-rib"
+ },
+ {
+   id: 25,
+   table: 3,
+   text: "Maureen Usman",
+   meal: "Short-rib"
+ },
+ {
+   id: 26,
+   table: 6,
+   text: "Samuel Priyatna",
+   meal: "Short-rib"
+ },
+ {
+   id: 27,
+   table: 6,
+   text: "Christopher",
+   meal: "Short-rib"
+ },
+ {
+   id: 28,
+   table: 0,
+   text: "Michael Wijaya",
+   meal: "Short-rib"
+ },
+ {
+   id: 29,
+   table: 0,
+   text: "Jessica Ekasalim",
+   meal: "Short-rib"
+ },
+ {
+   id: 30,
+   table: 2,
+   text: "Yudi chandra ",
+   meal: "Short-rib"
+ },
+ {
+   id: 31,
+   table: 2,
+   text: "Xuan Yao",
+   meal: "Short-rib"
+ },
+ {
+   id: 32,
+   table: 2,
+   text: "Hera susanto",
+   meal: "Short-rib"
+ },
+ {
+   id: 33,
+   table: 2,
+   text: "Mailina Kurniawan",
+   meal: "Short-rib"
+ },
+ {
+   id: 34,
+   table: 2,
+   text: "Ferdy Tjahjadi",
+   meal: "Short-rib"
+ },
+ {
+   id: 35,
+   table: 3,
+   text: "Nanda Kuswanto",
+   meal: "Short-rib"
+ },
+ {
+   id: 36,
+   table: 6,
+   text: "Gracia Sharon",
+   meal: "Salmon"
+ },
+ {
+   id: 37,
+   table: 6,
+   text: "Sinclair Kenrick",
+   meal: "Short-rib"
+ },
+ {
+   id: 38,
+   table: 1,
+   text: "Sehan Ekasalim",
+   meal: "Short-rib"
+ },
+ {
+   id: 39,
+   table: 1,
+   text: "Flora Susanto ",
+   meal: "Salmon"
+ },
+ {
+   id: 40,
+   table: 2,
+   text: "Donna Rota",
+   meal: "Salmon"
+ },
+ {
+   id: 41,
+   table: 2,
+   text: "Allen Rota",
+   meal: "Short-rib"
+ },
+ {
+   id: 42,
+   table: 8,
+   text: "Joshua Ekasalim",
+   meal: "Short-rib"
+ },
+ {
+   id: 43,
+   table: 5,
+   text: "Randy Yuwono",
+   meal: "Salmon"
+ },
+ {
+   id: 44,
+   table: 3,
+   text: "Robert Matusik",
+   meal: "Short-rib"
+ },
+ {
+   id: 45,
+   table: 7,
+   text: "Patricia de Paula e Freitas",
+   meal: "Chicken"
+ },
+ {
+   id: 46,
+   table: 7,
+   text: "Wlademir Nunes",
+   meal: "Salmon"
+ },
+ {
+   id: 47,
+   table: 7,
+   text: "Gabriela de Paula e Freitas Nunes",
+   meal: "Kid's meal"
+ },
+ {
+   id: 48,
+   table: 4,
+   text: "Jessica Wijaya",
+   meal: "Short-rib"
+ },
+ {
+   id: 49,
+   table: 5,
+   text: "Camellia Tan",
+   meal: "Short-rib"
+ },
+ {
+   id: 50,
+   table: 5,
+   text: "Edwin Chan",
+   meal: "Short-rib"
+ },
+ {
+   id: 51,
+   table: 4,
+   text: "Cornelia Maxine",
+   meal: "Salmon"
+ },
+ {
+   id: 52,
+   table: 4,
+   text: "Kartika Dewi",
+   meal: "Short-rib"
+ },
+ {
+   id: 53,
+   table: 4,
+   text: "Constantine Alexander Soelaiman ",
+   meal: "Salmon"
+ },
+ {
+   id: 54,
+   table: 5,
+   text: "Calista Tanaka",
+   meal: "Salmon"
+ },
+ {
+   id: 55,
+   table: 5,
+   text: "John Chua",
+   meal: "Short-rib"
+ },
+ {
+   id: 56,
+   table: 5,
+   text: "Michael Chow",
+   meal: "Short-rib"
+ },
+ {
+   id: 57,
+   table: 2,
+   text: "Hermanto Hioe",
+   meal: "Short-rib"
+ },
+ {
+   id: 58,
+   table: 2,
+   text: "Venlyna Phang",
+   meal: "Short-rib"
+ },
+ {
+   id: 59,
+   table: 8,
+   text: "Timothy Simanhadi",
+   meal: "Short-rib"
+ },
+ {
+   id: 60,
+   table: 7,
+   text: "Lucia Chonn",
+   meal: "Salmon"
+ },
+ {
+   id: 61,
+   table: 7,
+   text: "Jeff Soo",
+   meal: "Short-rib"
+ },
+ {
+   id: 62,
+   table: 7,
+   text: "Carlos Agena",
+   meal: "Short-rib"
+ },
+ {
+   id: 63,
+   table: 4,
+   text: "Rosalina Christiani",
+   meal: "Short-rib"
+ },
+ {
+   id: 64,
+   table: 4,
+   text: "Richard Bradley",
+   meal: "Short-rib"
+ },
+ {
+   id: 65,
+   table: 4,
+   text: "Wahyu Hidajati",
+   meal: "Short-rib"
+ },
+ {
+   id: 66,
+   table: 8,
+   text: "Otto Priadi",
+   meal: "Short-rib"
+ },
+ {
+   id: 67,
+   table: 8,
+   text: "Fernando Ng",
+   meal: "Salmon"
+ },
+ {
+   id: 68,
+   table: 3,
+   text: "Louis Zhang",
+   meal: "Short-rib"
+ },
+ {
+   id: 69,
+   table: 8,
+   text: "Adrian Vincent",
+   meal: "Short-rib"
+ },
+ {
+   id: 70,
+   table: 8,
+   text: "Haris",
+   meal: "Short-rib"
+ },
+ {
+   id: 71,
+   table: 5,
+   text: "Nadia Tan",
+   meal: "Short-rib"
+ },
+ {
+   id: 72,
+   table: 5,
+   text: "Joshua Salamun",
+   meal: "Salmon"
+ }
+]
